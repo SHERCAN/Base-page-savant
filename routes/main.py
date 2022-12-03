f1e8f5e5a7fba5302095e5cfc7dc49b7ab6eb50b
SHERCAN <sidereoyess@gmail.com>
2022-12-03 15:10:51 -0500

    hola lola

--- routes/main.py
+++ routes/main.py
@@ -10,10 +10,13 @@ from fastapi import Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import datetime as dt
+from starlette.status import HTTP_302_FOUND
 from fastapi import APIRouter
 from starlette.responses import RedirectResponse
 import pathlib
+from security.jwt_functions import validate_token, write_token
 import json
+from config.bd import dataBase
 from security.auth import auth_routes
 from classobject.classes import signupClass, Hash
 
@@ -26,6 +29,32 @@ templates = Jinja2Templates(directory='templates')
 async def signin(request: Request):
     context = {'request': request}
     response = templates.TemplateResponse('signin.html', context=context)
+    return response
+
+
+@main.post("/signin")
+async def login(email: str = Form(), password: str = Form(), request: Request = None):
+    try:
+        user = list(dataBase.readUser(id=email))
+    except:
+        # sin conexión base de datos
+        context = {'request': request, 'message': 'BD'}
+        response = templates.TemplateResponse(
+            'signin.html', context=context)
+        return response
+    if len(user) > 0:
+        if Hash.verify_password(password, user[0]['password']):
+            token = write_token({'email': email})
+            response = RedirectResponse('/', status_code=HTTP_302_FOUND)
+            response.set_cookie(key="Authorization", value=token)
+        else:
+            context = {'request': request, 'message': 'IP'}
+            response = templates.TemplateResponse(
+                'signin.html', context=context)
+    else:
+        context = {'request': request, 'message': 'ENT'}
+        response = templates.TemplateResponse(
+            'signin.html', context=context)
     return response
 
 
