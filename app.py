01fb6fa72d53be0390033daa2f7b86ccedf2681d
SHERCAN <sidereoyess@gmail.com>
2022-12-05 00:35:21 -0500

    FUNCTIONAL

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from fastapi.templating import Jinja2Templates
 from security.auth import auth_routes
 from routes.routes import routes
 from routes.main import main
-
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
 # -----------------------------------run---------------
 
 app = FastAPI(title='Server Enerion',
@@ -18,6 +20,10 @@ app.include_router(main)
 app.include_router(auth_routes, prefix='/api')
 app.include_router(routes)
 
+
 if __name__ == '__main__':
-    uvicorn.run('app:app', port=8000, log_level='info',
-                access_log=False, reload=True)
+    if getenv('MODE') == 'TEST':
+        uvicorn.run('app:app', log_level='info', access_log=False, reload=True)
+    else:
+        uvicorn.run(app='app:app', host='0.0.0.0', port=80,
+                    log_level='info', access_log=False)
